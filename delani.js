ec7e63742e3b1ee2c35a184d70c83a365e440f4f
Otieno100 <brian.masafu@student.moringaschool.com>
2022-03-13 18:03:32 +0300

    added more responsive jQuerry to make the site interractive

--- delani.js
+++ delani.js
@@ -7,10 +7,44 @@ $("#show").show()
 })
 
 })
+$(document).ready(function(){
+
+    $("#show").mouseover(function(){
+    
+    $(".toggle-1").toggle()
+    
+    })
+    
+    })
 
 
-$("#show").click(function(){
 
-$(".toggle-1").toggle()
+    $(document).ready(function(){
 
-})
+        $("#toggle").click(function(){
+        
+        $("#show-1").show()
+        
+        })
+        
+        })
+        $(document).ready(function(){
+        
+            $("#show-1").mouseover(function(){
+            
+            $(".toggle-2").toggle()
+            
+            })
+            
+            })
+
+            
+    $(document).ready(function(){
+
+        $("#toggle-2").click(function(){
+        
+        $("#show-2").show()
+        
+        })
+        
+        })
